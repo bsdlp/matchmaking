8d2252c5cd6b61dc6b742a0e9971b414564a794a
Jon Chen <bsd@voltaire.sh>
2015-08-01 00:22:22 -0700

    use NewConfigFromNamespace

--- matchmaking/config.go
+++ matchmaking/config.go
@@ -1,28 +1,16 @@
 package matchmaking
 
-import "gopkg.in/fly/config.v1"
+import "github.com/fly/config"
 
 // Config describes the config object..
 type Config struct {
 	TeamPlayerCount int      `yaml:"team_player_count"`
 	TeamCount       int      `yaml:"teams"`
-	Plugins         []string `yaml:"plugins"`
-}
-
-func NewConfigFromNamespace(organization string, system string) (c *Config, err error) {
-	var cfgNS = config.Namespace{
-		Organization: "fly",
-		system:       "matchmaking",
-	}
-	err = cfgNS.Load(c)
-	if err != nil {
-		return
-	}
-	return
+	Checks          []string `yaml:"checks"`
 }
 
 // NewConfig reads configuration from environment and returns a config object.
-func NewConfig() (c *Config, err error) {
-	c, err = NewConfigFromNamespace("fly", "matchmaking")
+func NewConfig() (c *config.Config, err error) {
+	c, err = config.NewConfigFromNamespace("fly", "matchmaking")
 	return
 }
