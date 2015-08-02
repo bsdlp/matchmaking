674cc32ec298996e6de9eee1b6230e857096c3a2
Jon Chen <bsd@voltaire.sh>
2015-08-01 17:21:27 -0700

    TeamPlayerCount -> TeamSize

--- cfg/config.go
+++ cfg/config.go
@@ -2,7 +2,7 @@ package cfg
 
 // Config describes the config object..
 type Config struct {
-	TeamPlayerCount int      `yaml:"team_player_count"`
-	TeamCount       int      `yaml:"teams"`
-	Checks          []string `yaml:"checks"`
+	TeamSize  int      `yaml:"team_size"`
+	TeamCount int      `yaml:"teams"`
+	Checks    []string `yaml:"checks"`
 }
