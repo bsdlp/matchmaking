b9a147205136453254a9cdcecc04d03488ef1854
Jon Chen <bsd@voltaire.sh>
2015-08-01 17:12:03 -0700

    remove config.NewConfig()
    let consumer implement NewConfig()

--- cfg/config.go
+++ cfg/config.go
@@ -1,19 +1,8 @@
 package cfg
 
-import "github.com/fly/config"
-
 // Config describes the config object..
 type Config struct {
 	TeamPlayerCount int      `yaml:"team_player_count"`
 	TeamCount       int      `yaml:"teams"`
 	Checks          []string `yaml:"checks"`
-}
-
-// NewConfig reads configuration from environment and returns a config object.
-func NewConfig() (c *config.Config, err error) {
-	c, err = config.NewConfigFromNamespace("fly", "matchmaking")
-	if err != nil {
-		return
-	}
-	return
 }
