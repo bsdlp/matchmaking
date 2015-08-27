1b369b026bbc340e8028eaa0b09cc0294076cdd0
Jon Chen <bsd@voltaire.sh>
2015-08-27 01:58:27 -0700

    update config object for irc bot

--- cfg/config.go
+++ cfg/config.go
@@ -4,22 +4,25 @@ import "github.com/fly/config"
 
 // Config describes the config object..
 type Config struct {
-	TeamSize     int          `yaml:"team_size"`
-	TeamCount    int          `yaml:"teams"`
-	Checks       []string     `yaml:"checks"`
-	IRCBotConfig IRCBotConfig `yaml:"irc_bot"`
+	TeamSize  int          `yaml:"team_size"`
+	TeamCount int          `yaml:"teams"`
+	Checks    []string     `yaml:"checks"`
+	IRC       IRCBotConfig `yaml:"irc_bot"`
 }
 
 // IRCBotConfig holds the config options of the matchmaking IRC bot
 type IRCBotConfig struct {
+	// hostname:port of IRC server to connect to.
 	Server string `yaml:"server"`
-	Port   int    `yaml:"port"`
+	// Name of the bot
 	Name   string `yaml:"name"`
+	UseTLS bool   `yaml:"use_tls"`
 	// password to connect to the IRC network
-	Password string `yaml:"password"`
-	Channels []struct {
+	Password      string `yaml:"password"`
+	CommandPrefix string `yaml:"command_prefix"`
+	Channels      []struct {
 		// name of the channel to join
-		Channel string `yaml:"channel"`
+		Name string `yaml:"Name"`
 		// password for entering the channel
 		Password string `yaml:"password"`
 	} `yaml:"channels"`
